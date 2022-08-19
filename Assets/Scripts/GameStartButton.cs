e148a9f10a4a22ea39a3f9275aab9dbc1ff743d4
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-19 14:40:08 +0900

    ゲームを始めるボタン実装

---
+++ Assets/Scripts/GameStartButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartButton : MonoBehaviour
+{
+    public void Pushed(){
+        // Mainシーンに遷移させる
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+}
