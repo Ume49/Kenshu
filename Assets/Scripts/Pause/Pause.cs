273917e1e69d1e9d46e56482f3f0958ad274cc8a
Ume49 <61038270+Ume49@users.noreply.github.com>
2022-08-19 18:06:17 +0900

    ポーズ実装

--- Assets/Scripts/Pause/Pause.cs
+++ Assets/Scripts/Pause/Pause.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class Pause : MonoBehaviour
 {
@@ -19,24 +20,88 @@ public class Pause : MonoBehaviour
         }
     }
 
-    [SerializeField] MonoBehaviour[] freezeComponent_;
+    // ゲームを停止するのに必要なもの
+    [SerializeField] ManagerScript      manager_;
+    [SerializeField] EmitterScript      emitter_;
+    [SerializeField] BackGroundScript[] backGrounds_;
+    [SerializeField] AudioSource        bgm_;
+    [Header("以下動的に確保するので入れなくていい")]
+    [SerializeField] PlayerScript[]     players_;
+    [SerializeField] SpaceShipScript[]  ships_;
+    [SerializeField] EnemyScript[]      enemies_;
+    [SerializeField] BulletScript[]     bullets_;
+    // 物理と止める直前の速度。RigidBody2D.enabled = falseと書けなかったため速度を0にする
+    [SerializeField] (Rigidbody2D R, Vector2 V)[]      rigidbodies;    
 
     // ゲーム停止処理
     void Freeze()
     {   Debug.Log("Pause.Freeze()");
 
-        foreach(var w in freezeComponent_){
-            w.enabled = false;
+        // 動的に変わるものを取得
+        players_    = FindObjectsOfType<PlayerScript>();
+        enemies_    = FindObjectsOfType<EnemyScript>();
+        ships_      = FindObjectsOfType<SpaceShipScript>();
+        bullets_    = FindObjectsOfType<BulletScript>();
+        rigidbodies = FindObjectsOfType<Rigidbody2D>().Select(w => (w, Vector2.zero)).ToArray();
+
+        // 動作を停止させる
+        Time.timeScale = 0;
+
+        manager_.enabled = false;
+        emitter_.enabled = false;
+        
+        foreach(var w in ships_)        { w.enabled=false; }
+        foreach(var w in bullets_)      { w.enabled=false; }
+        foreach(var w in backGrounds_)  { w.enabled=false; }
+        foreach(var w in enemies_)      { w.enabled=false; }
+        foreach(var w in players_)      { w.enabled=false; }
+
+        // BGMも一時停止
+        bgm_.Pause();
+
+        // 速度を0に
+        // 直前の速度は保存しておく
+        for(var i = 0; i<rigidbodies.Count(); i++){
+            // もともとの速度を保存
+            rigidbodies[i].V = rigidbodies[i].R.velocity;
+
+            // 速度を0に
+            rigidbodies[i].R.velocity = Vector2.zero;
         }
     }
 
     // ゲーム再開処理
+    // 基本的に停止処理の逆をやる
+    // 動的な確保はしない（ちゃんと停止されてるなら既に取った参照以上にオブジェクトが増えたり減ったりしてないはず）
     void UnFreeze()
     {   Debug.Log("Pause.UnFreeze()");
+        
+        // 動作を再開
+        Time.timeScale = 1.0f;
 
-        foreach(var w in freezeComponent_){
-            w.enabled = true;
+        manager_.enabled = true;
+        emitter_.enabled = true;
+
+        foreach(var w in ships_)        { w.enabled = true; }
+        foreach(var w in bullets_)      { w.enabled = true; }
+        foreach(var w in backGrounds_)  { w.enabled = true; }
+        foreach(var w in enemies_)      { w.enabled = true; }
+        foreach(var w in players_)      { w.enabled = true; }
+
+        // BGM再開
+        bgm_.UnPause();
+
+        // 速度をもとに戻す
+        for(var i = 0; i<rigidbodies.Count(); i++){
+            rigidbodies[i].R.velocity = rigidbodies[i].V;
         }
+
+        // 再開したら弾の情報とかが変わるのでその辺の配列は消しておく
+        players_    = null;
+        enemies_    = null;
+        ships_      = null;
+        bullets_    = null;
+        rigidbodies = null;
     }
 
     // ボタンで呼ぶ用のポーズ解除処理
